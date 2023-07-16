d7a0f419e109ff6d9e24a01be693eecbc5f85f3f
apolisskaya <alla.polisskaya@niche.com>
2023-07-15 21:30:19 -0700

    Add social icons

--- scripts/hello.js
+++ scripts/hello.js
@@ -1,13 +1,27 @@
+// Function to type 'Alla Spears' on the screen
 function typeHello() {
     const text = 'Alla Spears';
+    const textElement = document.getElementById('text-content');
+    const icons = document.querySelector('.social-icons');
+    const cursor = document.getElementById('cursor');
     let index = 0;
     const interval = setInterval(() => {
-        document.querySelector('h1').innerHTML += text[index];
+        cursor.style.visibility = 'visible'; // Show the cursor when typing starts
+        textElement.innerHTML += text[index];
         index++;
         if (index === text.length) {
             clearInterval(interval);
+            cursor.style.visibility = 'hidden'; // Hide the cursor after typing is completed
+            icons.style.opacity = 1; // fade social icons into view
         }
     }, 200);
 }
 
-window.onload = typeHello;
+window.onload = function () {
+    typeHello();
+
+    // Add the Font Awesome script here (in case it's not already loaded)
+    const fontAwesomeScript = document.createElement('script');
+    fontAwesomeScript.src = 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/js/all.min.js';
+    document.head.appendChild(fontAwesomeScript);
+};
